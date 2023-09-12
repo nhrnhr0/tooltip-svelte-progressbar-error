01ff44dfd54d9c2d54b9202dcd52b425bd990ed2
nhrnhr0 <nhrnhr0@gmail.com>
2023-09-13 00:20:50 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+import ProgressBar from "@okrad/svelte-progressbar";
+import { tooltip } from "../lib/tooltip";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="wraper" use:tooltip title="test title">
+  <ProgressBar series={[20, 20]} />
+</div>
